39081a5e2db2964554195bd9e471f48faf8c2689
Catto <nek0f3mboy@gmail.com>
2022-04-20 00:02:59 +0200

    🔊 Updated Messages

--- centos.sh
+++ centos.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -; sudo yum install -y nodejs
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -; sudo yum install -y nodejs; sudo yum install gtk3-devel; echo -e "\n\033[92mSuccessfully installed NodeJS 16 & GTK3-DEVEL \033[39m"
